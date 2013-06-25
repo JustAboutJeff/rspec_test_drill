ce455f64d7ee2dd8d9ed9f0ddf744c19f2a70b15
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-25 09:50:20 -0500

    Initial commit

---
+++ account_spec.rb
@@ -0,0 +1,64 @@
+require "rspec"
+require_relative "account"
+
+describe Account do
+  let(:my_account) { Account.new("1023456789",100) }
+
+  context "#initialize" do
+    it "should be an instance of Account" do
+      my_account.should be_an_instance_of Account
+    end
+
+    it "should set account number" do
+      my_account.acct_number.should_not be_nil
+    end
+
+    it "should accept one argument" do
+      expect { Account.new("4324324232") }.to_not raise_error
+    end
+
+    it "acct_number should be numeric" do
+      expect { Account.new("abi") }.to raise_error InvalidAccountNumberError
+    end
+  end
+
+  context "#transactions" do
+    it "should default to zero" do
+      Account.new("4523456723").transactions.should eq [0]
+    end
+
+    it "should return all the transactions" do
+      other_account = Account.new("4523356723",12)
+      other_account.deposit!(20)
+      other_account.transactions.should eq [12,20]
+    end
+  end
+
+  context "#balance" do
+    it "should return correct balance sum" do
+      other_account = Account.new("4523356723",12)
+      other_account.deposit!(20)
+      other_account.balance.should eq 32
+    end
+  end
+
+  context "#account_number" do
+    it "should hide the account number" do
+      my_account.acct_number.should_not eq "1023456789" 
+    end
+
+  end
+
+  context "deposit!" do
+    it "should fail for negative deposits" do
+      expect { my_account.deposit!(-10) }.to raise_error NegativeDepositError
+    end
+  end
+
+  context "#withdraw!" do
+    it "should raise error for overdraft" do
+      expect { my_account.withdraw!(101) }.to raise_error OverdraftError
+    end
+  end
+
+end
